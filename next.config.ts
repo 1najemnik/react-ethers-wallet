ef0c88e990a34ca6529caf1061927a23be00fda8
Ilya Gordon <ilyagdn@gmail.com>
2024-12-07 20:50:35 -0800

    chore(config): update Next.js configuration for GitHub Pages deployment

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    output: 'export',
+    output: "export",
+    basePath: process.env.NODE_ENV === 'production' ? '/react-ethers-wallet' : '',
+    assetPrefix: process.env.NODE_ENV === 'production' ? '/react-ethers-wallet' : '',
     trailingSlash: true,
+    reactStrictMode: true,
+    images: {
+        unoptimized: true,
+    },
 };
 
 export default nextConfig;
